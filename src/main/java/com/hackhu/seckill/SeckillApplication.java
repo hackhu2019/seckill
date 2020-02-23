692b0f2e131bf2ac6fd98cdb858b4d6310286556
hanyalun <hanyl@ops.best>
2020-02-23 23:41:34 +0800

    add maven dependencies and configuration mybatis

--- src/main/java/com/hackhu/seckill/SeckillApplication.java
+++ src/main/java/com/hackhu/seckill/SeckillApplication.java
@@ -1,9 +1,11 @@
 package com.hackhu.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hackhu.seckill"})
+@MapperScan("com.hackhu.seckill.dao")
 public class SeckillApplication {
 
     public static void main(String[] args) {
