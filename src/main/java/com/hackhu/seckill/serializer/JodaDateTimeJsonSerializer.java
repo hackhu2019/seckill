f48dc0d4ef3f4776f2aa337d700f5a12b5ab1119
hackhu2019 <hack_hu@163.com>
2020-03-18 20:30:10 +0800

    add DateTime.class json serializer and deserializer

---
+++ src/main/java/com/hackhu/seckill/serializer/JodaDateTimeJsonSerializer.java
@@ -0,0 +1,20 @@
+package com.hackhu.seckill.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.joda.time.DateTime;
+
+import java.io.IOException;
+
+/**
+ * @author hackhu
+ * @date 2020/3/18
+ */
+public class JodaDateTimeJsonSerializer extends JsonSerializer<DateTime> {
+
+    @Override
+    public void serialize(DateTime dateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(dateTime.toString("yyyy-MM-dd HH:mm:ss"));
+    }
+}
